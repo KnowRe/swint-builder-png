7ed3a5debc355bb677a0ce9a6f95e64e48250142
Kivol <kivoloid@gmail.com>
2015-08-27 18:39:20 +0900

    Alpha version complete

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # swint-builder-png
 PNG sprite builder for Swint
+
+**Warning: This is not the final draft yet, so do not use this until its official version is launched**
+
+## Installation
+```sh
+$ npm install --save swint-builder-png
+```
+
+## Options
+* `name` : `String`, default: `Project`
+* `inDir` : `String`, default: `path.dirname(require.main.filename)`
+* `outDir` : `String`, default: `path.join(path.dirname(require.main.filename), '../out')`
+* `imgMetaDir` : `String`, default: `path.join(path.dirname(require.main.filename), '../imgMeta')`
+* `walkOption` : `Object`, default: `{ ext: '*' }`
+
+## Usage
+```javascript
+buildPNG({
+	name: 'Test',
+	inDir: path.join(__dirname, 'png'),
+	outDir: path.join(__dirname, 'out'),
+	imgMetaDir: path.join(__dirname, 'imgMeta')
+}, function() {
+	// Build complete
+});
+```
---
+++ test/index.js
@@ -0,0 +1,62 @@
+var os = require('os'),
+	fs = require('fs'),
+	path = require('path'),
+	assert = require('assert'),
+	swintHelper = require('swint-helper'),
+	buildPNG = require('../lib');
+
+describe('builder-png', function() {
+	it('Error when no callback', function() {
+		assert.throws(function() {
+			buildPNG({});
+		});
+	});
+
+	it('Error when inDir doesn\'t exist', function(done) {
+		buildPNG({
+			inDir: '/this-directory-does-not-exist'
+		}, function(err, res) {
+			assert.notEqual(err, null);
+			done();
+		});
+	});
+
+	it('Simple case', function(done) {
+		buildPNG({
+			name: 'Test',
+			inDir: path.join(__dirname, '../test_case'),
+			imgMetaDir: path.join(os.tmpdir(), 'swint-builder-png-meta'),
+			outDir: path.join(os.tmpdir(), 'swint-builder-png-out')
+		}, function(err, res) {
+			assert.deepEqual(
+				fs.readFileSync(path.join(__dirname, '../test_result/meta/flags.meta.json')),
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-png-meta/flags.meta.json'))
+			);
+
+			assert.deepEqual(
+				fs.readFileSync(path.join(__dirname, '../test_result/meta/browsers.meta.json')),
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-png-meta/browsers.meta.json'))
+			);
+
+			assert.deepEqual(
+				fs.readFileSync(path.join(__dirname, '../test_result/out/flags.png')),
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-png-out/flags.png'))
+			);
+
+			assert.deepEqual(
+				fs.readFileSync(path.join(__dirname, '../test_result/out/browsers.png')),
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-png-out/browsers.png'))
+			);
+			done();
+		});
+	});
+
+	after(function() {
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-png-meta/flags.meta.json'));
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-png-meta/browsers.meta.json'));
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-png-out/flags.png'));
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-png-out/browsers.png'));
+		fs.rmdirSync(path.join(os.tmpdir(), 'swint-builder-png-meta'));
+		fs.rmdirSync(path.join(os.tmpdir(), 'swint-builder-png-out'));
+	});
+});
